83799f70b64ef2367b9cfa8a429bdde1ff6a362c
Jim-Clack <jim.clack@ablestrategies.com>
2024-11-21 18:09:07 -0500

    Added comments, changed package name

--- src/main/java/com/jim/inst/PreMain.java
+++ src/main/java/com/jim/inst/PreMain.java
@@ -4,7 +4,6 @@ import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
 import java.lang.instrument.ClassFileTransformer;
-import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
 
